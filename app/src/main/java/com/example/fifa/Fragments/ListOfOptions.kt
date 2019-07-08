4d41fa7bd9597ea1a8b73246975af95b1cdf4fb8
Maciej Sabik <maciej.sabik@apator.com>
2019-07-08 15:10:35 +0200

    Added dialog window to exitButton in ListOfOptions fragment

--- app/src/main/java/com/example/fifa/Fragments/ListOfOptions.kt
+++ app/src/main/java/com/example/fifa/Fragments/ListOfOptions.kt
@@ -1,6 +1,7 @@
 package com.example.fifa.Fragments
 
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.fifa.R
+import kotlinx.android.synthetic.main.fragment_list_of_options.view.*
+import kotlin.system.exitProcess
 
 class ListOfOptions : Fragment() {
 
@@ -15,7 +18,33 @@ class ListOfOptions : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_list_of_options, container, false)
+        val view = inflater.inflate(R.layout.fragment_list_of_options, container, false)
+
+        view.listOfPlayersButton.setOnClickListener {
+
+        }
+
+        view.loadPlayersButton.setOnClickListener {
+
+        }
+
+        view.exitButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this.context)
+
+            builder.setTitle("Exit")
+            builder.setMessage("Are you sure you want to exit?")
+            builder.setPositiveButton("Yes") {dialog, which ->
+                activity!!.finish()
+            }
+            builder.setNegativeButton("No") {dialog, which ->
+            }
+
+            val dialog: AlertDialog = builder.create()
+
+            dialog.show()
+        }
+
+        return view
     }
 
 
