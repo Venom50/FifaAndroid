9e03ff0e7774d6b491dbc5365801a76793b10579
Maciej Sabik <maciej.sabik@apator.com>
2019-07-10 11:28:19 +0200

    Added new Views in player_info.xml

--- app/src/main/java/com/example/fifa/Fragments/PlayerInfo.kt
+++ app/src/main/java/com/example/fifa/Fragments/PlayerInfo.kt
@@ -14,12 +14,17 @@ import com.example.fifa.Models.Player
 import com.example.fifa.R
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_player_info.view.*
+import java.text.FieldPosition
 
 class PlayerInfo : Fragment() {
 
     lateinit var nameTextView: TextView
     lateinit var idTextView: TextView
     lateinit var photoImageView: ImageView
+    lateinit var overallTextView: TextView
+    lateinit var positionTextView: TextView
+    lateinit var clubLogoImageView: ImageView
+    lateinit var flagImageView: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,10 +37,18 @@ class PlayerInfo : Fragment() {
         nameTextView = view.nameTextView
         idTextView = view.idTextView
         photoImageView = view.photoImageView
+        overallTextView = view.overallTextView
+        positionTextView = view.positionTextView
+        clubLogoImageView = view.clubLogoImageView
+        flagImageView = view.flagImageView
 
         nameTextView.text = player.name
         idTextView.text = player.id.toString()
         Picasso.get().load(player.photo).resize(140, 140).into(photoImageView)
+        overallTextView.text = player.overall.toString()
+        positionTextView.text = player.position
+        Picasso.get().load(player.clubLogo).resize(60, 60).into(clubLogoImageView)
+        Picasso.get().load(player.flag).resize(60, 40).into(flagImageView)
 
         return view
     }
